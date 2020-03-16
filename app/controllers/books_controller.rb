bc34a791de73a8292279a5eb6f14cc32ab311173
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-16 21:16:50 +0900

    [Fix]deviseによるサインイン、サインアウト時の遷移先ページの変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+    before_action :authenticate_user!
+
 	def index
 		@books = Book.all
 		@book = Book.new
@@ -10,7 +12,7 @@ class BooksController < ApplicationController
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		@book.save
-		redirect_to books_path
+		redirect_to book_path(@book.id)
 	end
 	def show
 		@book = Book.find(params[:id])
