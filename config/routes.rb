6f5a8130435e2135de0bbb981ef0c1ee92d80e38
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-16 15:33:32 +0900

    [Add]削除、編集機能の追加。各種リンクの設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books, only: [:new,:create,:index,:show]
+  resources :books, only: [:new,:create,:index,:show,:destroy,:edit,:update]
 end
