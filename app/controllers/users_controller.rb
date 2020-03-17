ee3495bde6eb4a80003fcafa7d3810746a82778d
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-17 18:55:07 +0900

    [Add]各箇所ヴァリデーションチェックとエラーメッセージの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,14 @@ class UsersController < ApplicationController
   end
   def update
   	@user = User.find(params[:id])
-  	@user.update(user_params)
-  	redirect_to user_path(@user.id)
+
+    if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully."
+  	   redirect_to user_path(@user.id)
+    else
+       render :edit
+    end
+
   end
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
