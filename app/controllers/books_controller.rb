2ee26cfe2f728e69c387df00b3bbe2ef1b419171
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-17 20:27:10 +0900

    [Add]book/showに投稿ユーザーへのリンクを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,18 @@ class BooksController < ApplicationController
 		@users = User.all
 	end
 	def create
+		@books = Book.all
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		@book.save
+		@user = current_user
+		@users = User.all
+		if @book.save
+		flash[:notice] = "You have created book successfully."
 		redirect_to book_path(@book.id)
+		else
+		render :index
+		end
+
 	end
 	def show
 		@book = Book.find(params[:id])
@@ -28,8 +36,12 @@ class BooksController < ApplicationController
 	end
 	def update
 		@book = Book.find(params[:id])
-		@book.update(book_params)
+		if @book.update(book_params)
+		flash[:notice] = "You have updated book successfully."
 		redirect_to book_path(@book.id)
+		else
+		render :edit
+		end
 	end
 
 	private
